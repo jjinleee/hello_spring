850a46b2d6747d00a3c1ece3afd4283700986890
jjinlee <dlwls6877@naver.com>
2024-02-16 14:27:21 +0900

    회원웹기능-등록

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hello_spring.controller;
 
+import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -11,5 +14,20 @@ public class MemberController {
     @Autowired  //컨트롤러와 서비스를 연결하기 위함
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member=new Member();
+        member.setName(form.getName());  //private으로 정의된 name을 가져옴
+
+        memberService.join(member);
+
+        return "redirect:/";  //회원가입이끝나고 홈화면으로 돌아가도록
     }
 }
